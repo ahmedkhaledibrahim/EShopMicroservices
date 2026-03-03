565d6392b08fdb11226f07347c391cf98f10f44d
Ahmed Khaled <58346988+ahmedkhaledibrahim@users.noreply.github.com>
2026-03-03 15:30:19 +0200

    Add project files.

---
+++ Catalog.Application/Interfaces/ICommon/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.Application.Interfaces.ICommon
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        public IBaseRepository<T> GetRepository<T>() where T : class;
+        public Task SaveChangesAsync();
+
+    }
+}
